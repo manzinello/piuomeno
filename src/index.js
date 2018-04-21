f769966780d186bf8cbb972d886a8e757fba5b8f
Matteo Manzinello <matteo@manzinello.com>
2018-04-21 14:40:01 +0200

    Aggiunto css all'index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 import PiuomenoApp from './PiuomenoApp';
 
 ReactDOM.render(<PiuomenoApp />, document.getElementById('root'));
