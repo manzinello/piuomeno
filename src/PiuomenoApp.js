be7629723d29dcec64cc0b5fe3322e5385a87a8a
Matteo Manzinello <matteo@manzinello.com>
2018-04-21 14:38:16 +0200

    Aggiunti un po' di elementi da Grommet

--- src/PiuomenoApp.js
+++ src/PiuomenoApp.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 
 import App from 'grommet/components/App'
 import Header from 'grommet/components/Header';
+import Tabs from 'grommet/components/Tabs';
+import Tab from 'grommet/components/Tab';
+import Paragraph from 'grommet/components/Paragraph';
 import Title from 'grommet/components/Title';
-import Box from 'grommet/components/Box';
+import Section from 'grommet/components/Section';
 import NumberInput from 'grommet/components/NumberInput';
 
 class PiuomenoApp extends Component {
@@ -19,19 +22,29 @@ class PiuomenoApp extends Component {
           <Title>
             Più o meno
           </Title>
-          <Box flex={true}
-            justify='end'
-            direction='row'
-            responsive={true}>
-            <p>Testo</p>
-          </Box>
         </Header>
-        <NumberInput value={10}
-          onChange={this.handleChange} />
+        <Tabs>
+          <Tab title='First Title'>
+            <Paragraph>
+              First contents
+    </Paragraph>
+            <input type='text'
+              value=''
+              onChange={this.handleChange} />
+          </Tab>
+          <Tab title='Second Title'>
+            <Paragraph>
+              Second contents
+    </Paragraph>
+            <input type='text'
+              value=''
+              onChange={this.handleChange} />
+          </Tab>
+        </Tabs>
       </App>
     );
   }
-  
+
 }
 
 export default PiuomenoApp;
