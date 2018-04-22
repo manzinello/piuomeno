782d06b3ff31011f2a698212fc0a73c5585f3ad6
Matteo Manzinello <matteo@manzinello.com>
2018-04-22 10:36:40 +0200

    Inserita navigazione a tab

--- src/PiuomenoApp.js
+++ src/PiuomenoApp.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import App from 'grommet/components/App'
+import Tabs from 'grommet/components/Tabs';
+import Tab from 'grommet/components/Tab';
+
 import Header from 'grommet/components/Header';
 import Title from 'grommet/components/Title';
 import Section from 'grommet/components/Section';
@@ -42,24 +45,34 @@ class PiuomenoApp extends Component {
             Più o meno
           </Title>
         </Header>
-        <Section>
-          <Label>
-            Chilogrammi
+        <Tabs responsive={false}
+          justify='start'>
+          <Tab title='Peso'>
+            <Section>
+              <Label>
+                Chilogrammi
             </Label>
-          <NumberInput
-            onChange={this.handleChange} />
-        </Section>
-        <Section>
-          <Paragraph size='large'>
-            <Emoji e="🐘" /><Emoji e="🇨🇬" /> ~<strong>{this.state.elefantiafricani.toFixed(2)}</strong> elefanti africani
+              <NumberInput
+                onChange={this.handleChange} />
+            </Section>
+            <Section>
+              <Paragraph size='large'>
+                <Emoji e="🐘" /><Emoji e="🇨🇬" /> ~<strong>{this.state.elefantiafricani.toFixed(2)}</strong> elefanti africani
             <br />
-            <Emoji e="🐘" /><Emoji e="🇨🇳" /> ~<strong>{this.state.elefantiasiatici.toFixed(2)}</strong> elefanti asiatici
+                <Emoji e="🐘" /><Emoji e="🇨🇳" /> ~<strong>{this.state.elefantiasiatici.toFixed(2)}</strong> elefanti asiatici
             <br />
-            <Emoji e="🐘" /><Emoji e="🌳" /> ~<strong>{this.state.elefantiforeste.toFixed(2)}</strong> elefanti delle foreste
+                <Emoji e="🐘" /><Emoji e="🌳" /> ~<strong>{this.state.elefantiforeste.toFixed(2)}</strong> elefanti delle foreste
             <br />
-            <Emoji e="🐳" /> ~<strong>{this.state.balenottereazzurre.toFixed(2)}</strong> balenottere azzurre
+                <Emoji e="🐳" /> ~<strong>{this.state.balenottereazzurre.toFixed(2)}</strong> balenottere azzurre
           </Paragraph>
-        </Section>
+            </Section>
+          </Tab>
+          <Tab title='Distanza'>
+            <Paragraph>
+              Work in progress!
+            </Paragraph>
+          </Tab>
+        </Tabs>
         <Footer justify='between'
           size='medium'>
           <Box direction='row'
