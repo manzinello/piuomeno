31a988c87ae9912d020512cb9fd203c7e3d03461
Matteo Manzinello <matteo@manzinello.com>
2018-10-02 19:37:35 +0200

    Aggiunto package.json per i components

--- src/PiuomenoApp.js
+++ src/PiuomenoApp.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import App from 'grommet/components/App'
-import Tabs from 'grommet/components/Tabs';
-import Tab from 'grommet/components/Tab';
+import App from "grommet/components/App";
+import Tabs from "grommet/components/Tabs";
+import Tab from "grommet/components/Tab";
 
-import Header from 'grommet/components/Header';
-import Title from 'grommet/components/Title';
-import Section from 'grommet/components/Section';
-import NumberInput from 'grommet/components/NumberInput';
-import Paragraph from 'grommet/components/Paragraph';
-import Footer from 'grommet/components/Footer';
-import Box from 'grommet/components/Box';
-import Label from 'grommet/components/Label';
-import Select from 'grommet/components/Select';
+import Header from "grommet/components/Header";
+import Title from "grommet/components/Title";
+import Section from "grommet/components/Section";
+import NumberInput from "grommet/components/NumberInput";
+import Paragraph from "grommet/components/Paragraph";
+import Footer from "grommet/components/Footer";
+import Box from "grommet/components/Box";
+import Label from "grommet/components/Label";
+import Select from "grommet/components/Select";
 
-import Emoji from './components/Emoji'
+import Emoji from "@components/Emoji";
 
 // In chilogrammi
 const PESO_FORMICA = 0.000006;
@@ -43,7 +43,6 @@ const OCEANO_PACIFICO = 161800000000000;
 const TERRA = 510100000000000;
 
 class PiuomenoApp extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -67,11 +66,11 @@ class PiuomenoApp extends Component {
       rinocerontenero: 0,
       rinoceronteindiano: 0,
       rinocerontebianco: 0,
-      terre: 0,
+      terre: 0
     };
   }
 
-  handleChangePeso = (event) => {
+  handleChangePeso = event => {
     this.setState({
       elefantiafricani: event.target.value / PESO_ELEFANTE_AFRICANO,
       elefantiasiatici: event.target.value / PESO_ELEFANTE_ASIATICO,
@@ -82,11 +81,11 @@ class PiuomenoApp extends Component {
       formiche: event.target.value / PESO_FORMICA,
       rinocerontenero: event.target.value / PESO_RINOCERONTE_NERO,
       rinoceronteindiano: event.target.value / PESO_RINOCERONTE_INDIANO,
-      rinocerontebianco: event.target.value / PESO_RINOCERONTE_BIANCO,
-    })
-  }
+      rinocerontebianco: event.target.value / PESO_RINOCERONTE_BIANCO
+    });
+  };
 
-  handleChangeSuperficie = (event) => {
+  handleChangeSuperficie = event => {
     this.setState({
       campidacalcio: event.target.value / CAMPO_DA_CALCIO,
       campidabasket: event.target.value / CAMPO_DA_BASKET,
@@ -94,24 +93,23 @@ class PiuomenoApp extends Component {
       italia: event.target.value / ITALIA,
       oceaniatlantici: event.target.value / OCEANO_ATLANTICO,
       oceanipacifici: event.target.value / OCEANO_PACIFICO,
-      terre: event.target.value / TERRA,
-    })
-  }
+      terre: event.target.value / TERRA
+    });
+  };
 
-  handleChangeLunghezza = (event) => {
+  handleChangeLunghezza = event => {
     this.setState({
       terraluna: event.target.value / DISTANZA_TERRA_LUNA,
       lunghezzacampocalcio: event.target.value / LUNGHEZZA_CAMPO_CALCIO,
-      piscinaolimpionicalunghezza: event.target.value / PISCINA_OLIMPIONICA,
-    })
-  }
+      piscinaolimpionicalunghezza: event.target.value / PISCINA_OLIMPIONICA
+    });
+  };
 
-  handleChangeVolume = (event) => {
-    console.log(event.target.value)
-  }
+  handleChangeVolume = event => {
+    console.log(event.target.value);
+  };
 
   render() {
-
     var elephant = "emoji/elephant.png";
     var africa = "emoji/africa.png";
     var india = "emoji/india.png";
@@ -134,122 +132,147 @@ class PiuomenoApp extends Component {
     var black = "emoji/black.png";
     var white = "emoji/white.png";
 
-
     return (
       <App>
-        <Header
-         size='large'>
-          <Title>
-            Più o meno ~
-          </Title>
+        <Header size="large">
+          <Title>Più o meno ~</Title>
         </Header>
-        <Tabs responsive={false}
-          justify='start'>
-          <Tab title='Peso'>
+        <Tabs responsive={false} justify="start">
+          <Tab title="Peso">
             <Section>
-              <Label>
-                Chilogrammi
-                </Label>
-              <NumberInput
-                onChange={this.handleChangePeso} />
+              <Label>Chilogrammi</Label>
+              <NumberInput onChange={this.handleChangePeso} />
             </Section>
             <Section>
-              <Paragraph size='large'>
-                <img src={formica} width="24" height="24" /> ~<strong>{this.state.formiche.toFixed(2)}</strong> formiche
+              <Paragraph size="large">
+                <img src={formica} width="24" height="24" /> ~
+                <strong>{this.state.formiche.toFixed(2)}</strong> formiche
                 <br />
-                <img src={gorilla} width="24" height="24" /> ~<strong>{this.state.gorilla.toFixed(2)}</strong> gorilla
+                <img src={gorilla} width="24" height="24" /> ~
+                <strong>{this.state.gorilla.toFixed(2)}</strong> gorilla
                 <br />
-                <img src={rino} width="24" height="24" /> <img src={black} width="24" height="24" /> ~<strong>{this.state.rinocerontenero.toFixed(2)}</strong> rinoceronti neri
+                <img src={rino} width="24" height="24" />{" "}
+                <img src={black} width="24" height="24" /> ~
+                <strong>{this.state.rinocerontenero.toFixed(2)}</strong>{" "}
+                rinoceronti neri
                 <br />
-                <img src={rino} width="24" height="24" /> <img src={india} width="24" height="24" />  ~<strong>{this.state.rinoceronteindiano.toFixed(2)}</strong> rinoceronti indiani
+                <img src={rino} width="24" height="24" />{" "}
+                <img src={india} width="24" height="24" /> ~
+                <strong>{this.state.rinoceronteindiano.toFixed(2)}</strong>{" "}
+                rinoceronti indiani
                 <br />
-                <img src={rino} width="24" height="24" /> <img src={white} width="24" height="24" />  ~<strong>{this.state.rinocerontebianco.toFixed(2)}</strong> rinoceronti bianchi
+                <img src={rino} width="24" height="24" />{" "}
+                <img src={white} width="24" height="24" /> ~
+                <strong>{this.state.rinocerontebianco.toFixed(2)}</strong>{" "}
+                rinoceronti bianchi
                 <br />
-                <img src={elephant} width="24" height="24" /> <img src={tree} width="24" height="24" /> ~<strong>{this.state.elefantiforeste.toFixed(2)}</strong> elefanti delle foreste
+                <img src={elephant} width="24" height="24" />{" "}
+                <img src={tree} width="24" height="24" /> ~
+                <strong>{this.state.elefantiforeste.toFixed(2)}</strong>{" "}
+                elefanti delle foreste
                 <br />
-                <img src={elephant} width="24" height="24" /> <img src={india} width="24" height="24" /> ~<strong>{this.state.elefantiasiatici.toFixed(2)}</strong> elefanti asiatici
+                <img src={elephant} width="24" height="24" />{" "}
+                <img src={india} width="24" height="24" /> ~
+                <strong>{this.state.elefantiasiatici.toFixed(2)}</strong>{" "}
+                elefanti asiatici
                 <br />
-                <img src={elephant} width="24" height="24" /> <img src={africa} width="24" height="24" /> ~<strong>{this.state.elefantiafricani.toFixed(2)}</strong> elefanti africani
+                <img src={elephant} width="24" height="24" />{" "}
+                <img src={africa} width="24" height="24" /> ~
+                <strong>{this.state.elefantiafricani.toFixed(2)}</strong>{" "}
+                elefanti africani
                 <br />
-                <img src={trex} width="24" height="24" /> ~<strong>{this.state.trex.toFixed(2)}</strong> T-Rex
+                <img src={trex} width="24" height="24" /> ~
+                <strong>{this.state.trex.toFixed(2)}</strong> T-Rex
                 <br />
-                <img src={whale} width="24" height="24" /> ~<strong>{this.state.balenottereazzurre.toFixed(2)}</strong> balenottere azzurre
-          </Paragraph>
-            </Section>
-          </Tab>
-          <Tab title='Lunghezza'>
-            <Section>
-              <Label>
-                Metri
-            </Label>
-              <NumberInput
-                onChange={this.handleChangeLunghezza} />
-            </Section>
-            <Section>
-              <Paragraph size='large'>
-                <img src={swimmer} width="24" height="24" /> ~<strong>{this.state.piscinaolimpionicalunghezza.toFixed(2)}</strong> lunghezza di una piscina olimpionica
-                <br />
-                <img src={soccer} width="24" height="24" /> ~<strong>{this.state.lunghezzacampocalcio.toFixed(2)}</strong> lunghezza di un campo da calcio
-                <br />
-                <img src={earth} width="24" height="24" /> <img src={moon} width="24" height="24" /> ~<strong>{this.state.terraluna.toFixed(2)}</strong> distanza Terra-Luna
-          </Paragraph>
-            </Section>
-          </Tab>
-          <Tab title='Superficie'>
-            <Section>
-              <Label>
-                Metri quadrati
-            </Label>
-              <NumberInput
-                onChange={this.handleChangeSuperficie} />
-            </Section>
-            <Section>
-              <Paragraph size='large'>
-                <img src={tennis} width="24" height="24" /> ~<strong>{this.state.campidatennis.toFixed(2)}</strong> campi da tennis
-                <br />
-                <img src={basket} width="24" height="24" /> ~<strong>{this.state.campidabasket.toFixed(2)}</strong> campi da basket
-                <br />
-                <img src={soccer} width="24" height="24" /> ~<strong>{this.state.campidacalcio.toFixed(2)}</strong> campi da calcio
-                <br />
-                <img src={italy} width="24" height="24" /> ~<strong>{this.state.italia.toFixed(2)}</strong> Italie 
-                 <br />
-                <img src={oceano} width="24" height="24" /> ~<strong>{this.state.oceaniatlantici.toFixed(2)}</strong> Oceani Atlantici 
-                 <br />
-                <img src={oceano} width="24" height="24" /> ~<strong>{this.state.oceanipacifici.toFixed(2)}</strong> Oceani Pacifici 
-                 <br />
-                <img src={earth} width="24" height="24" /> ~<strong>{this.state.terre.toFixed(2)}</strong> Terre
-          </Paragraph>
+                <img src={whale} width="24" height="24" /> ~
+                <strong>{this.state.balenottereazzurre.toFixed(2)}</strong>{" "}
+                balenottere azzurre
+              </Paragraph>
             </Section>
           </Tab>
-          <Tab title='Volume'>
+          <Tab title="Lunghezza">
             <Section>
-              <Label>
-                Metri cubi
-            </Label>
-              <NumberInput
-                onChange={this.handleChangeVolume} />
+              <Label>Metri</Label>
+              <NumberInput onChange={this.handleChangeLunghezza} />
             </Section>
             <Section>
-              <Paragraph size='large'>
-                Ci stiamo lavorando!
-          </Paragraph>
+              <Paragraph size="large">
+                <img src={swimmer} width="24" height="24" /> ~
+                <strong>
+                  {this.state.piscinaolimpionicalunghezza.toFixed(2)}
+                </strong>{" "}
+                lunghezza di una piscina olimpionica
+                <br />
+                <img src={soccer} width="24" height="24" /> ~
+                <strong>{this.state.lunghezzacampocalcio.toFixed(2)}</strong>{" "}
+                lunghezza di un campo da calcio
+                <br />
+                <img src={earth} width="24" height="24" />{" "}
+                <img src={moon} width="24" height="24" /> ~
+                <strong>{this.state.terraluna.toFixed(2)}</strong> distanza
+                Terra-Luna
+              </Paragraph>
+            </Section>
+          </Tab>
+          <Tab title="Superficie">
+            <Section>
+              <Label>Metri quadrati</Label>
+              <NumberInput onChange={this.handleChangeSuperficie} />
+            </Section>
+            <Section>
+              <Paragraph size="large">
+                <img src={tennis} width="24" height="24" /> ~
+                <strong>{this.state.campidatennis.toFixed(2)}</strong> campi da
+                tennis
+                <br />
+                <img src={basket} width="24" height="24" /> ~
+                <strong>{this.state.campidabasket.toFixed(2)}</strong> campi da
+                basket
+                <br />
+                <img src={soccer} width="24" height="24" /> ~
+                <strong>{this.state.campidacalcio.toFixed(2)}</strong> campi da
+                calcio
+                <br />
+                <img src={italy} width="24" height="24" /> ~
+                <strong>{this.state.italia.toFixed(2)}</strong> Italie
+                <br />
+                <img src={oceano} width="24" height="24" /> ~
+                <strong>{this.state.oceaniatlantici.toFixed(2)}</strong> Oceani
+                Atlantici
+                <br />
+                <img src={oceano} width="24" height="24" /> ~
+                <strong>{this.state.oceanipacifici.toFixed(2)}</strong> Oceani
+                Pacifici
+                <br />
+                <img src={earth} width="24" height="24" /> ~
+                <strong>{this.state.terre.toFixed(2)}</strong> Terre
+              </Paragraph>
+            </Section>
+          </Tab>
+          <Tab title="Volume">
+            <Section>
+              <Label>Metri cubi</Label>
+              <NumberInput onChange={this.handleChangeVolume} />
+            </Section>
+            <Section>
+              <Paragraph size="large">Ci stiamo lavorando!</Paragraph>
             </Section>
           </Tab>
         </Tabs>
-        <Footer justify='between'
-          size='medium'>
-          <Box direction='row'
-            align='center'
-            pad={{ "between": "medium" }}>
-            <Paragraph margin='none'>
-              creato con <img src={love} width="24" height="24" /> per <strong>Ilaria</strong> da <a href="https://matteomanzinello.com"><strong>Matteo</strong></a>
+        <Footer justify="between" size="medium">
+          <Box direction="row" align="center" pad={{ between: "medium" }}>
+            <Paragraph margin="none">
+              creato con <img src={love} width="24" height="24" /> per{" "}
+              <strong>Ilaria</strong> da{" "}
+              <a href="https://matteomanzinello.com">
+                <strong>Matteo</strong>
+              </a>
             </Paragraph>
           </Box>
         </Footer>
       </App>
     );
   }
-
 }
 
 export default PiuomenoApp;
