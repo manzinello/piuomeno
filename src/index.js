07f47399ebdea47a7abedeb936ff83c165d2e0dd
Matteo Manzinello <matteo@manzinello.com>
2018-10-04 15:09:48 +0200

    Commentato registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+
 import Piuomeno from "./Piuomeno";
 
 ReactDOM.render(<Piuomeno />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
