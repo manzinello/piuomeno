7eed6ac98454e485b9df01e1f072572494c16965
Matteo Manzinello <matteo@manzinello.com>
2019-08-08 12:34:42 +0200

    fix github corner

--- src/Piuomeno.js
+++ src/Piuomeno.js
@@ -15,6 +15,7 @@ import ValutaTab from "./tabs/ValutaTab";
 import PiuomenoFooter from "./components/PiuomenoFooter";
 
 import "./piuomeno.css";
+import GitHubCorner from "./components/GitHubCorner";
 
 class Piuomeno extends Component {
   state = {
@@ -35,29 +36,31 @@ class Piuomeno extends Component {
     var elephant = "emoji/elephant.png";
 
     return (
-      <App className="piuomeno-app">
-        <Header size="large">
-          <Title className="piuomeno-title">
-            <img alt="elephant" src={elephant} width="24" height="24" />
-            {"Più o meno"}
-          </Title>
-        </Header>
-        <Tabs responsive={true} justify="start">
-          <Tab title="Peso">
-            <PesoTab />
-          </Tab>
-          <Tab title="Lunghezza">
-            <LunghezzaTab />
-          </Tab>
-          <Tab title="Superficie">
-            <SuperficieTab />
-          </Tab>
-          <Tab title="Valuta">
-            <ValutaTab />
-          </Tab>
-        </Tabs>
-        <PiuomenoFooter />
-      </App>
+      <>
+        <App className="piuomeno-app">
+          <Header size="large">
+            <Title className="piuomeno-title">
+              <img alt="elephant" src={elephant} width="24" height="24" />
+              {"Più o meno"}
+            </Title>
+          </Header>
+          <Tabs responsive={true} justify="start">
+            <Tab title="Peso">
+              <PesoTab />
+            </Tab>
+            <Tab title="Lunghezza">
+              <LunghezzaTab />
+            </Tab>
+            <Tab title="Superficie">
+              <SuperficieTab />
+            </Tab>
+            <Tab title="Valuta">
+              <ValutaTab />
+            </Tab>
+          </Tabs>
+          <PiuomenoFooter />
+        </App>
+      </>
     );
   }
 }
